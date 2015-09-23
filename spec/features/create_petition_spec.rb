c4ecef388cc7799ae97585999d6b6fefa4419460
Jon Normington <jnormington@users.noreply.github.com>
2015-09-23 14:16:16 +0100

    Complete spec for creating petition  - Use the constituency helper for stubbing the api calls  - Move Alans additional spec within a for loop to dry it up a little.

--- spec/features/create_petition_spec.rb
+++ spec/features/create_petition_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Creating Petition', type: :feature do
+RSpec.describe 'Create Petition', type: :feature do
   before(:each) { visit home_url }
 
   describe 'with search' do
@@ -32,107 +32,60 @@ RSpec.describe 'Creating Petition', type: :feature do
     end
   end
 
-  it 'creates a pending petition with a pending signature and email' do
-    petition = {
-                 action: 'The wombats of wimbledon rock.',
-                 background: 'Give half of Wimbledon rock to wombats!',
-                 name: 'Womboid Wibbledon',
-                 email: 'womboid@wimbledon.com',
-                 postcode: 'E1 6PL',
-                 location: 'United Kingdom',
-                 additional_detail: 'The racial tensions between the wombles and the wombats are heating up.
-                   Racial attacks are a regular occurrence and the death count is already in 5 figures.
-                   The only resolution to this crisis is to give half of Wimbledon common to the Wombats and
-                   to recognise them as their own independent state.'
-               }
+  describe 'with valid detail' do
+    let(:petition) do
+      {
+        action: 'The wombats of wimbledon rock.',
+        background: 'Give half of Wimbledon rock to wombats!',
+        name: 'Womboid Wibbledon',
+        email: 'womboid@wimbledon.com',
+        postcode: '',
+        location: 'United Kingdom',
+        additional_detail: 'The racial tensions between the wombles and the wombats are heating up.
+               Racial attacks are a regular occurrence and the death count is already in 5 figures.
+               The only resolution to this crisis is to give half of Wimbledon common to the Wombats
+               and to recognise them as their own independent state.'
+      }
+    end
 
-    stub_request(:get, "http://data.parliament.uk/membersdataplatform/services/mnis/Constituencies/E16PL/").
-      with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v0.9.1'}).
-      to_return(:status => 200, :body => "", :headers => {})
+    before(:each) do
+      stub_any_api_request.to_return(api_response(200, 'single'))
+      ActionMailer::Base.deliveries.clear
+    end
 
-    visit new_petition_url
+    [{postcode: 'E1 6PL', type: :valid}, {postcode: 'AA11 1AA', type: :invalid}].each do |data|
+      it "creates a pending petition with signature when the postcode is #{data[:type]}" do
+        petition.merge!(postcode: data[:postcode])
 
-    expect(page).to have_title 'Start a petition - Petitions'
-    expect(page.current_url).to match(%r!^https://!)
+        visit new_petition_url
 
-    fill_in_all_petition_stages_with petition
+        expect(page).to have_title 'Start a petition - Petitions'
+        expect(page.current_url).to match(%r!^https://!)
 
-    expect(sanitize_page(page)).to be_valid_markup
-    expect(page).to have_content 'Make sure this is right'
-    expect(page).to have_field 'Email'
+        fill_in_all_petition_stages_with petition
 
-    click_button 'Yes – this is my email address'
-    expect(page).to have_text 'One more step…'
+        expect(sanitize_page(page)).to be_valid_markup
+        expect(page).to have_content 'Make sure this is right'
+        expect(page).to have_field 'Email'
 
-    last_petition = Petition.last
-    expect(last_petition.action).to eq 'The wombats of wimbledon rock.'
-    expect(last_petition.state).to eq 'pending'
+        click_button 'Yes – this is my email address'
+        expect(page).to have_text 'One more step…'
 
-    signature = Signature.where(email: petition[:email]).find_by(name: petition[:name], state: 'pending')
-    expect(signature).not_to be_nil
-    expect(signature.notify_by_email?).to be_truthy
+        last_petition = Petition.last
+        expect(last_petition.action).to eq 'The wombats of wimbledon rock.'
+        expect(last_petition.state).to eq 'pending'
 
-    # TODO: Get emails and assert
-    # on it look at the cuke definitions a little more
-    # open_last_email_for
+        signature = Signature.where(email: petition[:email]).find_by(name: petition[:name], state: 'pending')
+        expect(signature).not_to be_nil
+        expect(signature.notify_by_email?).to be_truthy
 
-    # email = ActionMailer::Base.deliveries.last
-    # expect(email.to).to eq petition[:email]
-    # expect(email.subject).to eq 'Action required: Petition'
-    # expect(email.body).to match(/\/petitions\/\\d+\/sponsors\/[A-Za-z0-9]+/)
+        expect(ActionMailer::Base.deliveries.size).to eq 1
+
+        open_email_for(petition[:email], subject: 'Action required: Petition')
+        current_email.to have_body(/\/petitions\/\\d+\/sponsors\/[A-Za-z0-9]+/)
+      end
+    end
   end
-  
-  it 'create a petition with invalid postcode' do
-    petition = {
-                 action: 'The wombats of wimbledon rock.',
-                 background: 'Give half of Wimbledon rock to wombats!',
-                 name: 'Womboid Wibbledon',
-                 email: 'womboid@wimbledon.com',
-                 postcode: 'AA11 1AA',
-                 location: 'United Kingdom',
-                 additional_detail: 'The racial tensions between the wombles and the wombats are heating up.
-                   Racial attacks are a regular occurrence and the death count is already in 5 figures.
-                   The only resolution to this crisis is to give half of Wimbledon common to the Wombats and
-                   to recognise them as their own independent state.'
-               }
-
-  # TODO: Move to better place
-    stub_request(:get, "http://data.parliament.uk/membersdataplatform/services/mnis/Constituencies/AA111AA/").
-      with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v0.9.1'}).
-      to_return(:status => 200, :body => "", :headers => {})
-
-    visit new_petition_url
-
-    expect(page).to have_title 'Start a petition - Petitions'
-    expect(page.current_url).to match(%r!^https://!)
-
-    fill_in_all_petition_stages_with petition
-
-    expect(sanitize_page(page)).to be_valid_markup
-    expect(page).to have_content 'Make sure this is right'
-    expect(page).to have_field 'Email'
-
-    click_button 'Yes – this is my email address'
-    expect(page).to have_text 'One more step…'
-
-    last_petition = Petition.last
-    expect(last_petition.action).to eq 'The wombats of wimbledon rock.'
-    expect(last_petition.state).to eq 'pending'
-
-    signature = Signature.where(email: petition[:email]).find_by(name: petition[:name], state: 'pending')
-    expect(signature).not_to be_nil
-    expect(signature.notify_by_email?).to be_truthy
-
-    # TODO: Get emails and assert
-    # on it look at the cuke definitions a little more
-    # open_last_email_for
-
-    # email = ActionMailer::Base.deliveries.last
-    # expect(email.to).to eq petition[:email]
-    # expect(email.subject).to eq 'Action required: Petition'
-    # expect(email.body).to match(/\/petitions\/\\d+\/sponsors\/[A-Za-z0-9]+/)
-  end
-  
 
   def click_start_petition
      within 'main#content' do
@@ -169,10 +122,4 @@ RSpec.describe 'Creating Petition', type: :feature do
     click_button 'This looks good'
     fill_in_signature_step_with(detail)
   end
-  
-  def check_postcode_with_gov_api(search_query)
-    stub_request(:get, "http://data.parliament.uk/membersdataplatform/services/mnis/Constituencies/" + search_query + "/").
-      with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v0.9.1'}).
-      to_return(:status => 200, :body => "", :headers => {})
-  end  
 end
