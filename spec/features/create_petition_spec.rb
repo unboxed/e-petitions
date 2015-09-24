baaaaa4b26b25078d0cfb7360488deb67ab6584a
Jon Normington <jnormington@users.noreply.github.com>
2015-09-24 14:56:11 +0100

    Add invalid detail and going back a step

--- spec/features/create_petition_spec.rb
+++ spec/features/create_petition_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Create Petition', type: :feature do
-  before(:each) { visit home_url }
+
+  before(:each) do
+    stub_any_api_request.to_return(api_response(200, 'single'))
+    visit home_url
+  end
+
   let(:petition) do
     {
       action: 'The wombats of wimbledon rock.',
@@ -10,10 +15,10 @@ RSpec.describe 'Create Petition', type: :feature do
       email: 'womboid@wimbledon.com',
       postcode: 'E1 6PL',
       location: 'United Kingdom',
-      additional_detail: 'The racial tensions between the wombles and the wombats are heating up.
-             Racial attacks are a regular occurrence and the death count is already in 5 figures.
-             The only resolution to this crisis is to give half of Wimbledon common to the Wombats
-             and to recognise them as their own independent state.'
+      additional_detail: 'The racial tensions between the wombles and the wombats are heating up.'\
+             'Racial attacks are a regular occurrence and the death count is already in 5 figures.'\
+             'The only resolution to this crisis is to give half of Wimbledon common to the Wombats'\
+             'and to recognise them as their own independent state.'
     }
   end
 
@@ -47,10 +52,6 @@ RSpec.describe 'Create Petition', type: :feature do
   end
 
   describe 'with valid detail' do
-    before(:each) do
-      stub_any_api_request.to_return(api_response(200, 'single'))
-    end
-
     [{postcode: 'E1 6PL', type: :valid}, {postcode: 'AA11 1AA', type: :invalid}].each do |data|
       it "creates a pending petition with signature when the postcode is #{data[:type]}" do
         petition.merge!(postcode: data[:postcode])
@@ -83,15 +84,43 @@ RSpec.describe 'Create Petition', type: :feature do
         current_email.to have_body(/\/petitions\/\\d+\/sponsors\/[A-Za-z0-9]+/)
       end
     end
+
+    it 'remembers users input when going back a stage' do
+      visit new_petition_url
+      fill_in_start_petition_step_with(petition)
+
+      expect(page).to have_css('h1', 'Check your petition')
+      expect(page).to have_text petition[:action]
+
+      find(:xpath, "//details/summary[contains(., 'More details')]").click
+      expect(page).to have_text petition[:additional_detail]
+
+      click_button 'Go back and make changes'
+
+      expect(page).to have_field('What do you want us to do?', with: petition[:action])
+      expect(page).to have_field('Background', with: petition[:background])
+      expect(page).to have_field('Additional details', with: petition[:additional_detail])
+
+      click_button 'Preview petition'
+      expect(page).to have_text petition[:action]
+
+      click_button 'This looks good'
+      fill_in_signature_step_with petition
+
+      click_button 'Back'
+
+      expect(page).to have_field('Name', with: petition[:name])
+      expect(page).to have_field('Email address', with: petition[:email])
+      expect(page).to have_field('Postcode', with: PostcodeSanitizer.call(petition[:postcode]))
+
+      select_field = find(:xpath, "id('petition_creator_signature_country')/option[@selected]")
+      expect(select_field.text).to eq petition[:location]
+    end
   end
 
   describe 'has its first sponsor' do
     let(:last_petition) { Petition.last }
     let(:sponsor) {{ email: FactoryGirl.generate(:sponsor_email) }}
-
-    before(:each) do
-      stub_any_api_request.to_return(api_response(200, 'single'))
-    end
 
     it 'validates the petition and signature' do
       visit new_petition_url
@@ -129,6 +158,58 @@ RSpec.describe 'Create Petition', type: :feature do
     end
   end
 
+  describe 'with invalid detail' do
+    let(:last_petition) { Petition.last }
+    let(:signature) { Signature.where(email: petition[:email]).find_by(name: petition[:name], state: 'pending') }
+
+    before(:each) { visit new_petition_url }
+
+    it 'doesnt create the petition with no input' do
+      click_button 'Preview petition'
+
+      expect(page).to have_text 'Action must be completed'
+      expect(page).to have_text 'Background must be completed'
+
+      fill_in_start_petition_step_with(petition)
+      click_button 'This looks good'
+      click_button 'Continue'
+
+      expect(page).to have_text 'Name must be completed'
+      expect(page).to have_text 'Email must be completed'
+      expect(page).to have_text 'You must be a British citizen'
+      expect(page).to have_text 'Postcode must be completed'
+    end
+
+    it 'doesnt create the petition with excessive characters' do
+      #additional_details has a soft limit even though the js shows minus count at 500 characters
+      fill_in_start_petition_step_with({action: 'a'*81, background: 'b'*301, additional_detail: 'c'*801 })
+      expect(page).to have_text 'Action is too long'
+      expect(page).to have_text 'Background is too long'
+      expect(page).to have_text 'Additional details is too long'
+
+      fill_in_start_petition_step_with(petition)
+      click_button 'This looks good'
+
+      expect(page).to have_css('h1', 'Sign your petition')
+      fill_in_signature_step_with(name: 'a'*256, email: 'b'*256, postcode: 'c'*256, nocheck: true)
+      click_button 'Continue'
+
+      expect(page).to have_text 'You must use your real name'
+      expect(page).to have_text 'You must be a British citizen or normally live in the UK to create or sign petitions'
+      expect(page).to have_text 'You must be a British citizen'
+      expect(page).to have_text 'Postcode not recognised'
+
+      fill_in_signature_step_with(petition)
+      expect(page).to have_text 'Make sure this is right'
+      click_button 'Yes – this is my email address'
+
+      expect(last_petition.action).to eq petition[:action]
+      expect(last_petition.state).to eq 'pending'
+      expect(signature).to be_present
+      expect(signature.state).to eq 'pending'
+    end
+  end
+
   def click_start_petition
      within 'main#content' do
       click_link 'Start a petition'
@@ -151,7 +232,7 @@ RSpec.describe 'Create Petition', type: :feature do
   def fill_in_signature_step_with(detail = {})
     fill_in 'Name', with: detail[:name] || 'Ben'
     fill_in 'Email', with: detail[:email]
-    check 'I am a British citizen or UK resident'
+    check 'I am a British citizen or UK resident' unless detail[:nocheck]
 
     fill_in 'Postcode', with: detail[:postcode] || 'E1 6PL'
     select detail[:location] || 'United Kingdom', from: 'Location'
