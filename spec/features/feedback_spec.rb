8a02074a7065d3e0ec5cc00851d712dce4e1df50
Alan Thomas <alan.thomas@unboxedconsulting.com>
2015-10-08 16:08:56 +0100

    Use let for feedback email

--- spec/features/feedback_spec.rb
+++ spec/features/feedback_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Feedback', type: :feature do
   let(:feedback) { Feedback.new(name: 'Joe Public', email: 'foo@example.com', comment: "I can't submit a petition for some reason", petition_link_or_title: 'link') }
+  let(:petitionemail) { 'petitionscommittee@parliament.uk' }
   before(:each) { visit feedback_url }
   
   it 'Submit feedback' do
@@ -12,8 +13,8 @@ RSpec.describe 'Feedback', type: :feature do
     click_button("Send feedback")
     expect(page).to have_content("Thank")
     
-    expect(unread_emails_for('petitionscommittee@parliament.uk').size).to eq parse_email_count(1)
-    open_email('petitionscommittee@parliament.uk')
+    expect(unread_emails_for(petitionemail).size).to eq parse_email_count(1)
+    open_email(petitionemail)
     expect(current_email.default_part_body.to_s).to include(feedback.petition_link_or_title)
     expect(current_email.default_part_body.to_s).to include(feedback.email)
     expect(current_email.default_part_body.to_s).to include(feedback.comment)
@@ -22,6 +23,6 @@ RSpec.describe 'Feedback', type: :feature do
   it 'Check required fields' do
     click_button("Send feedback")
     expect(page).to have_content("must be completed")
-    expect(unread_emails_for('petitionscommittee@parliament.uk').size).to eq parse_email_count(0)
+    expect(unread_emails_for(petitionemail).size).to eq parse_email_count(0)
   end
 end      
