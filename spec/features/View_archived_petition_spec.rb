027090b972d6bb70ecfd3e293497a71d46a5b87e
Alan Thomas <alan.thomas@unboxedconsulting.com>
2015-10-13 16:24:25 +0100

    View links and urls and rejected archived petitions

--- spec/features/View_archived_petition_spec.rb
+++ spec/features/View_archived_petition_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'View archived petition', type: :feature do
   
-  it 'view an archived petition' do
+  it 'that is closed' do
     @petition = FactoryGirl.create(:archived_petition, :closed, title: 'Spend more money on Defence')
     visit archived_petition_url(@petition)
     expect(page).to have_content(@petition.title)
@@ -12,7 +12,7 @@ RSpec.describe 'View archived petition', type: :feature do
     expect(page).to have_content('This petition has been archived It was submitted during the 2010–2015 Conservative – Liberal Democrat coalition government')
   end
   
-  it 'go to old URL' do
+  it 'via the old URL' do
     @petition = FactoryGirl.create(:archived_petition, :closed, title: 'Spend more money on Defence')
     visit petition_url(@petition)
     expect(current_path).to eq(archived_petition_path(@petition))
@@ -21,5 +21,29 @@ RSpec.describe 'View archived petition', type: :feature do
     expect(page).to have_title('Spend more money on Defence'.to_s)
     expect(page).to have_css("li.meta-deadline", :text => "Deadline " + @petition.closed_at.strftime("%e %B %Y").squish)
     expect(page).to have_content('This petition has been archived It was submitted during the 2010–2015 Conservative – Liberal Democrat coalition government')
-  end 
+  end
+  
+  it 'that contains urls, email addresses and html tags' do
+    @petition = FactoryGirl.create(:archived_petition, :closed, title: 'Spend more money on Defence', description: "<i>We<i> like http://www.google.com and bambi@gmail.com")
+    visit archived_petition_url(ArchivedPetition.find_by(title: 'Spend more money on Defence'))
+    expect(page).to have_content("<i>We<i>")
+    expect(page).to have_link("http://www.google.com", href: "http://www.google.com")
+    expect(page).to have_link("bambi@gmail.com", href: "mailto:bambi@gmail.com")
+  end  
+
+  it 'that is rejected with a reason' do
+    @petition = FactoryGirl.create(:archived_petition, :rejected, title: "Please bring back Eldorado", reason_for_rejection: "<i>We<i> like http://www.google.com and bambi@gmail.com")
+    visit archived_petition_url(@petition)
+    expect(page).to have_content(@petition.title)
+    expect(page).to have_content(@petition.description)
+    expect(page).to have_title('Please bring back Eldorado'.to_s)
+    @petition.reload
+    expect(page).to have_content(@petition.reason_for_rejection)
+    expect(page).to have_content("<i>We<i>")
+    expect(page).to have_link("http://www.google.com", href: "http://www.google.com")
+    expect(page).to have_link("bambi@gmail.com", href: "mailto:bambi@gmail.com")
+    expect(page).to_not have_content("0 signatures")
+    expect(page).to_not have_content('Deadline')
+    expect(page).not_to have_css("a", :text => "Sign")
+  end
 end
