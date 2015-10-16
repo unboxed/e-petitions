ab0734b6087010155f15b91b89b11229cd1ebd78
Alan Thomas <alan.thomas@unboxedconsulting.com>
2015-10-16 15:33:12 +0100

    Style updates to feedback spec

--- spec/features/feedback_spec.rb
+++ spec/features/feedback_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Feedback', type: :feature do
-  let(:feedback) { Feedback.new(name: 'Joe Public', email: 'foo@example.com', comment: "I can't submit a petition for some reason", petition_link_or_title: 'link') }
+  let(:feedback) { Feedback.new(name: 'Joe Public', email: 'foo@example.com',
+                 comment: "I can't submit a petition for some reason", petition_link_or_title: 'link') }
   let(:petitionemail) { 'petitionscommittee@parliament.uk' }
   before(:each) { visit feedback_url }
   
-  it 'Submit feedback' do
+  it 'is submitted' do
     fill_in "feedback[email]", :with => feedback.email
     fill_in "feedback[petition_link_or_title]", :with => feedback.petition_link_or_title
     fill_in "feedback[comment]", :with => feedback.comment
@@ -20,7 +21,7 @@ RSpec.describe 'Feedback', type: :feature do
     expect(current_email.default_part_body.to_s).to include(feedback.comment)
   end
   
-  it 'Check required fields' do
+  it 'is checked for required fields' do
     click_button("Send feedback")
     expect(page).to have_content("must be completed")
     expect(unread_emails_for(petitionemail).size).to eq parse_email_count(0)
