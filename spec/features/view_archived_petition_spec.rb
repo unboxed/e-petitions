281e08899d015a9f18eaa03b7ce3fdb88e91e8da
Alan Thomas <alan.thomas@unboxedconsulting.com>
2015-10-22 16:31:48 +0100

    Remove to_s and reload for archived petitions

--- spec/features/view_archived_petition_spec.rb
+++ spec/features/view_archived_petition_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'View archived petition', type: :feature do
     visit archived_petition_url(petition)
     expect(page).to have_content(petition.title)
     expect(page).to have_content(petition.description)
-    expect(page).to have_title('Spend more money on Defence'.to_s)
+    expect(page).to have_title('Spend more money on Defence')
     expect(page).to have_css("li.meta-deadline", :text => "Deadline " + petition.closed_at.strftime("%e %B %Y").squish)
     expect(page).to have_content(archive_message)
   end
@@ -19,7 +19,7 @@ RSpec.describe 'View archived petition', type: :feature do
     expect(current_path).to eq(archived_petition_path(petition))
     expect(page).to have_content(petition.title)
     expect(page).to have_content(petition.description)
-    expect(page).to have_title('Spend more money on Defence'.to_s)
+    expect(page).to have_title('Spend more money on Defence')
     expect(page).to have_css("li.meta-deadline", :text => "Deadline " + petition.closed_at.strftime("%e %B %Y").squish)
     expect(page).to have_content(archive_message)
   end
@@ -36,8 +36,7 @@ RSpec.describe 'View archived petition', type: :feature do
     visit archived_petition_url(rejected_petition)
     expect(page).to have_content(rejected_petition.title)
     expect(page).to have_content(rejected_petition.description)
-    expect(page).to have_title('Please bring back Eldorado'.to_s)
-    rejected_petition.reload
+    expect(page).to have_title('Please bring back Eldorado')
     expect(page).to have_content(rejected_petition.reason_for_rejection)
     expect(page).to have_content("<i>We<i>")
     expect(page).to have_link("http://www.google.com", href: "http://www.google.com")
@@ -52,7 +51,7 @@ RSpec.describe 'View archived petition', type: :feature do
     expect(current_path).to eq(archived_petition_path(petition))
     expect(page).to have_content(petition.title)
     expect(page).to have_content(petition.description)
-    expect(page).to have_title('Spend more money on Defence'.to_s)
+    expect(page).to have_title('Spend more money on Defence')
     expect(page).not_to have_css("a", :text => "Sign")
   end
   
