b6155095ec8442e5d23f8a98be4f8485425a778f
Alan Thomas <alan.thomas@unboxedconsulting.com>
2015-10-22 15:18:21 +0100

    Move factories to lets for archived petitions

--- spec/features/view_archived_petition_spec.rb
+++ spec/features/view_archived_petition_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'View archived petition', type: :feature do
   let(:archive_message) {'This petition has been archived It was submitted during the 2010–2015 Conservative – Liberal Democrat coalition government'}
+  let(:petition) {@petition = FactoryGirl.create(:archived_petition, :closed, title: 'Spend more money on Defence')}
+  let(:rejected_petition) {@petition = FactoryGirl.create(:archived_petition, :rejected, title: "Please bring back Eldorado", reason_for_rejection: "<i>We<i> like http://www.google.com and bambi@gmail.com")}
   
   it 'that is closed' do
-    @petition = FactoryGirl.create(:archived_petition, :closed, title: 'Spend more money on Defence')
-    visit archived_petition_url(@petition)
-    expect(page).to have_content(@petition.title)
-    expect(page).to have_content(@petition.description)
+    visit archived_petition_url(petition)
+    expect(page).to have_content(petition.title)
+    expect(page).to have_content(petition.description)
     expect(page).to have_title('Spend more money on Defence'.to_s)
-    expect(page).to have_css("li.meta-deadline", :text => "Deadline " + @petition.closed_at.strftime("%e %B %Y").squish)
+    expect(page).to have_css("li.meta-deadline", :text => "Deadline " + petition.closed_at.strftime("%e %B %Y").squish)
     expect(page).to have_content(archive_message)
   end
   
   it 'via the old URL' do
-    @petition = FactoryGirl.create(:archived_petition, :closed, title: 'Spend more money on Defence')
-    visit petition_url(@petition)
-    expect(current_path).to eq(archived_petition_path(@petition))
-    expect(page).to have_content(@petition.title)
-    expect(page).to have_content(@petition.description)
+    visit petition_url(petition)
+    expect(current_path).to eq(archived_petition_path(petition))
+    expect(page).to have_content(petition.title)
+    expect(page).to have_content(petition.description)
     expect(page).to have_title('Spend more money on Defence'.to_s)
-    expect(page).to have_css("li.meta-deadline", :text => "Deadline " + @petition.closed_at.strftime("%e %B %Y").squish)
+    expect(page).to have_css("li.meta-deadline", :text => "Deadline " + petition.closed_at.strftime("%e %B %Y").squish)
     expect(page).to have_content(archive_message)
   end
   
@@ -33,13 +33,12 @@ RSpec.describe 'View archived petition', type: :feature do
   end  
 
   it 'that is rejected with a reason' do
-    @petition = FactoryGirl.create(:archived_petition, :rejected, title: "Please bring back Eldorado", reason_for_rejection: "<i>We<i> like http://www.google.com and bambi@gmail.com")
-    visit archived_petition_url(@petition)
-    expect(page).to have_content(@petition.title)
-    expect(page).to have_content(@petition.description)
+    visit archived_petition_url(rejected_petition)
+    expect(page).to have_content(rejected_petition.title)
+    expect(page).to have_content(rejected_petition.description)
     expect(page).to have_title('Please bring back Eldorado'.to_s)
-    @petition.reload
-    expect(page).to have_content(@petition.reason_for_rejection)
+    rejected_petition.reload
+    expect(page).to have_content(rejected_petition.reason_for_rejection)
     expect(page).to have_content("<i>We<i>")
     expect(page).to have_link("http://www.google.com", href: "http://www.google.com")
     expect(page).to have_link("bambi@gmail.com", href: "mailto:bambi@gmail.com")
@@ -49,18 +48,16 @@ RSpec.describe 'View archived petition', type: :feature do
   end
   
   it 'and not be able to sign it' do
-    @petition = FactoryGirl.create(:archived_petition, :closed, title: 'Spend more money on Defence')
-    visit archived_petition_url(@petition)
-    expect(current_path).to eq(archived_petition_path(@petition))
-    expect(page).to have_content(@petition.title)
-    expect(page).to have_content(@petition.description)
+    visit archived_petition_url(petition)
+    expect(current_path).to eq(archived_petition_path(petition))
+    expect(page).to have_content(petition.title)
+    expect(page).to have_content(petition.description)
     expect(page).to have_title('Spend more money on Defence'.to_s)
     expect(page).not_to have_css("a", :text => "Sign")
   end
   
   it 'and see the closed message' do
-    @petition = FactoryGirl.create(:archived_petition, :closed, title: 'Spend more money on Defence')
-    visit archived_petition_url(@petition)
+    visit archived_petition_url(petition)
     expect(page).to have_content(archive_message)
   end
 end
